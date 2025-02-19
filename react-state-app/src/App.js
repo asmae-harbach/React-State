d2435b8c07e20f38f5579b1206585378b0fb1896
asmae-harbach <asmaharbach@gmail.com>
2025-02-19 19:34:55 +0100

    react state

--- react-state-app/src/App.js
+++ react-state-app/src/App.js
@@ -1,34 +1,49 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
+
+
 
 class App extends Component {
   state = { 
     name: 'Asmaâ Harbach',
     email: 'asmaharbach@gmail.com', 
     img: '/logo512.png', 
-    montre: true
+    montre: true,
+    temp : 0
   };
 
-  change = ()=>{
+  affichage = ()=>{
     this.setState({ montre: !this.state.montre });
+    clearInterval(this.setState(() => ({ temp: 0})));
   };
 
+  componentDidMount(){
+    this.interval = setInterval(() => {
+      this.setState(() => ({ temp: this.state.temp + 1 }));
+    }, 1000);
+  }
+  
+  
   render() {
     {if(this.state.montre){
       return (
         <>
+          <h1>{this.state.temp}</h1>
           <p>{this.state.name}</p>
           <p>{this.state.email}</p>
-          <img src={this.state.img} alt="Person" />
-          <button onClick={this.change}>Change</button>
+          <img style={{width:'200px'}} src={this.state.img} alt="Person" />
+          <button onClick={this.affichage}>Change</button>
         </>
       )
     }}
     return(
-      <button onClick={this.change}>Change</button>
+      <>
+        <h1>{this.state.temp}</h1>
+        <button onClick={this.affichage}>Change</button>
+      </>
 
     )
   }
   
 }
 
-export default App;
+export default App
